381cd602575dd4070971f3cd53d2317f4c064e74
dhood27-a11y <hooddeja43@gmail.com>
2026-01-21 14:53:31 -0500

    added search feature to dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import TrendCard from "./components/TrendCard";
 
 function App() {
   const [trends, setTrends] = useState([]); //created a use state variable that will hold data
+  const [searchTerm, setSearchTerm] = useState(""); //will track user input
 
   useEffect(() => {
     //this will run once the site opens and will call api to "fetch" trends from backend
@@ -13,6 +14,19 @@ function App() {
     };
     getData();
   }, []);
+
+  //This function will check both title and source for a match
+
+  const filteredTrends = trends.filter((item) => {
+    const titleMatch = item.title
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+    const sourceMatch = item.source
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase());
+
+    return titleMatch || sourceMatch;
+  });
 
   return (
     <>
@@ -26,15 +40,39 @@ function App() {
       >
         {/*This will display to user how many articles were found exactly */}
         <h1>Trend Arbitrage Dashboard</h1>
+
+        {/* Search input field */}
+        <div style={{ marginBottom: "20px" }}>
+          <input
+            type="text"
+            placeholder="Search by title or source (e.g. 'Reddit)..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            style={{
+              width: "100%",
+              padding: "12px",
+              borderRadius: "8px",
+              border: "1px solid #ccc",
+              fontSize: "16px",
+            }}
+          />
+        </div>
+
         <p>
-          Top {trends.length} trending articles across Reddit, HackerNews, and
-          Google News
+          Showing {filteredTrends.length} of {trends.length} across Reddit,
+          HackerNews, and Google News
         </p>
         <br></br>
 
         {/*Here we are looping through our list and where a TrendCard will be created for each item */}
         {trends.length > 0 ? (
-          trends.map((item, index) => <TrendCard key={index} trend={item} />)
+          filteredTrends.length > 0 ? (
+            filteredTrends.map((item, index) => (
+              <TrendCard key={index} trend={item} />
+            ))
+          ) : (
+            <p>No results found for "{searchTerm}"</p>
+          )
         ) : (
           <p>Loading trends from MongoDB....</p>
         )}
